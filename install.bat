e10908d04694bdecacd09f48b392c0d4422cc0c3
KitSG <qq331315621@163.com>
2023-02-16 10:37:47 +0800

    EAtest

---
+++ install.bat
@@ -0,0 +1,3 @@
+pip install mkdocs -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install mkdocs-material -i https://pypi.tuna.tsinghua.edu.cn/simple
+git init
---
+++ upload.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "commit"
+git push -u git@github.com:PCTL0896/Class1.github.io.git main
+mkdocs gh-deploy --clean
