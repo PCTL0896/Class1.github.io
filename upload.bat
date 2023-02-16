2540fb1cfd8800d6c9d7c8e52fdf5e359abc6042
KitSG <qq331315621@163.com>
2023-02-16 10:33:01 +0800

    EAtest

--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "commit"
-git push -u git@github.com:PCTL0896/Class1-Wiki.github.io.git master
+git push -u git@github.com:PCTL0896/Class1.github.io.git main
 mkdocs gh-deploy --clean
